c0ca72fe361bbb9f3beaaf85b667f4984f7fff9e
ivan <ivandjp@gmail.com>
2017-04-01 18:00:26 +0800

    add ignore

---
+++ src/main/java/Computer.java
@@ -0,0 +1,35 @@
+import java.util.function.Function;
+
+/**
+ * Function Pro Example
+ * Created by ivan on 17-4-1.
+ **/
+public class Computer {
+
+    public static Integer compute(Function<Integer, Integer> function, Integer value) {
+
+        return function.apply(value);
+
+    }
+
+    private static Integer invert(Integer value) {
+
+        return -value;
+
+    }
+
+    public static Integer invertTheNumber() {
+
+        Integer toInvert = -5;
+
+        Function<Integer, Integer> invertFunction = Computer::invert;
+
+        return compute(invertFunction, toInvert);
+
+    }
+
+    public static void main(String[] args) {
+        Integer i = Computer.invertTheNumber();
+        System.out.println("------Value Print-------i Value=" + i + ", " + "With Caller=Computer.main");
+    }
+}
